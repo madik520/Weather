719f23f2c7bac385335784eecb0e00d20629b8f7
madik520 <ilyshik919@gmail.com>
2020-04-07 17:51:39 +0300

    Updates

---
+++ precache-manifest.4d015571a0ea998ca474f62d3ef3caf4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dceee61fcd5a5f60951f74774a3369f7",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "d93b1894cd01dcbbf5b5",
+    "url": "/Weather/static/css/main.3e16d1e5.chunk.css"
+  },
+  {
+    "revision": "c9660dfd94271e4e93ef",
+    "url": "/Weather/static/js/2.7e771103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.7e771103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93b1894cd01dcbbf5b5",
+    "url": "/Weather/static/js/main.f2e2b4f1.chunk.js"
+  },
+  {
+    "revision": "e70a3d3864c131219672",
+    "url": "/Weather/static/js/runtime-main.d5344606.js"
+  },
+  {
+    "revision": "b5213dc1d02bb9dd0b2f0ea64f5ca8a2",
+    "url": "/Weather/static/media/bg.b5213dc1.jpg"
+  },
+  {
+    "revision": "6bd531c532efb29ad641629ebef34cc9",
+    "url": "/Weather/static/media/icon.6bd531c5.png"
+  },
+  {
+    "revision": "45cad5d514870b0a8521f0740eb3ebea",
+    "url": "/Weather/static/media/sky-sun.45cad5d5.png"
+  },
+  {
+    "revision": "5585c03caf7389b7364be93e61070b6e",
+    "url": "/Weather/static/media/sun.5585c03c.png"
+  }
+]);
